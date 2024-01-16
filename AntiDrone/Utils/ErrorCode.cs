b2e26f61c45be31958defbe8456522c13fafa6d2
yjkang <yjkang@agos.co.kr>
2024-01-16 17:26:28 +0900

    240116 에러 코드 추가 및 메세지 주석처리

--- AntiDrone/Utils/ErrorCode.cs
+++ AntiDrone/Utils/ErrorCode.cs
@@ -14,12 +14,18 @@ public class Error
     public static void ErrorMessage()
     {
         ErrorCode CanNotWrite = ErrorCode.CanNotWrite;
-        Console.WriteLine("데이터 작성에 실패했습니다.");
+        //"데이터 작성에 실패했습니다."
 
         ErrorCode InvalidError = ErrorCode.InvalidError;
-        Console.WriteLine("알 수 없는 에러.");
+        //"알 수 없는 에러."
 
+        ErrorCode FeatureNotExist = ErrorCode.FeatureNotExist;
+        //"현재 지원하지 않는 기능입니다."
+        
         ErrorCode NotFound = ErrorCode.NotFound;
-        Console.WriteLine("요청에 대한 응답을 찾을 수 없습니다.");
+        //요청에 대한 응답을 찾을 수 없습니다.
+        
+        ErrorCode NoAuthority = ErrorCode.NoAuthority;
+        //"접근 권한이 없습니다."
     }
 }
