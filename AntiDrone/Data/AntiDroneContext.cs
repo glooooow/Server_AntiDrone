02e6656b11e3b943b9510f7e7477009cabc9bb5e
yjkang <yjkang@agos.co.kr>
2024-03-15 09:22:37 +0900

    240315 스캐너 탐지 모델 DB화 목록 추가(context 등록)

--- AntiDrone/Data/AntiDroneContext.cs
+++ AntiDrone/Data/AntiDroneContext.cs
@@ -21,8 +21,13 @@ namespace AntiDrone.Data
                 .HaveConversion<DateOnlyConverter>();
         }
         
+        // 관제 시스템 데이터 모델 -> DB화
         public DbSet<AntiDrone.Models.Systems.DroneControl.Whitelist> Whitelist { get; set; } = default!;
         public DbSet<AntiDrone.Models.Systems.Member.Member> Member { get; set; } = default!;
         public DbSet<AntiDrone.Models.Systems.Member.MemberLog> MemberLog { get; set; } = default!;
+
+        // 탐지 데이터 모델 -> DB화
+        public DbSet<AntiDrone.Models.Detections.ScannerDetections> ScannerDetections { get; set; } = default!;
+        public DbSet<AntiDrone.Models.Detections.HistoryHeader> HistoryHeader { get; set; } = default!;
     }
 }
