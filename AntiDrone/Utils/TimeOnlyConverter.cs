8c12c7d21aba9cae98dde8e580b1227eafaa521d
yjkang <yjkang@agos.co.kr>
2024-03-18 09:54:22 +0900

    240318 timeonly 타입 호출 에러 문제 해결(converter 내 타입 수정)

--- AntiDrone/Utils/TimeOnlyConverter.cs
+++ AntiDrone/Utils/TimeOnlyConverter.cs
@@ -2,11 +2,12 @@
 
 namespace AntiDrone.Utils;
 
-public class TimeOnlyConverter : ValueConverter<TimeOnly, DateTime>
+public class TimeOnlyConverter : ValueConverter<TimeOnly, TimeSpan>
 {
     public TimeOnlyConverter() : base(
-        timeOnly => new DateTime().AddTicks(timeOnly.Ticks),
-        dateTime => TimeOnly.FromDateTime(dateTime))
+        timeOnly => new TimeSpan(timeOnly.Ticks),
+        timeSpan => TimeOnly.FromTimeSpan(timeSpan))
+        //dateTime => TimeOnly.FromDateTime(dateTime))
     {
     }
 }
