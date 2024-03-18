9d9fa88d79b7d7d603f4d6e0f2db876f5f9a93aa
yjkang <yjkang@agos.co.kr>
2024-03-18 10:02:02 +0900

    240318 TimeOnlyConverter 수정 전 내용 주석 제거

--- AntiDrone/Utils/TimeOnlyConverter.cs
+++ AntiDrone/Utils/TimeOnlyConverter.cs
@@ -7,7 +7,6 @@ public class TimeOnlyConverter : ValueConverter<TimeOnly, TimeSpan>
     public TimeOnlyConverter() : base(
         timeOnly => new TimeSpan(timeOnly.Ticks),
         timeSpan => TimeOnly.FromTimeSpan(timeSpan))
-        //dateTime => TimeOnly.FromDateTime(dateTime))
     {
     }
 }
